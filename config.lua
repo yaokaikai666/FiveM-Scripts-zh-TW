706d4556d5248e62d0b366e11c0af887a090c509
永森集團 <97763242+yaokaikai666@users.noreply.github.com>
2023-08-24 02:28:31 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,37 @@
+
+
+Config = {}
+Config.OpenPhone = 'f1'   --## 顯示按鍵 ## https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.Phones = {"phone"}  -- # 物品名稱
+Config.Locale = 'en'
+Config.Fahrenheit = false --溫度計
+
+Config.ESXVersion = "1.2" -- 1.1 OR 1.2
+
+
+-- Discord WebHook - start --
+Config.Carseller = '你的WebHook' --車輛交易
+Config.JobNotif = '你的WebHook' --工作通知
+Config.TwitterWeb = '你的WebHook' --推特
+Config.YellowWeb = '你的WebHook' --廣告
+Config.InstagramWeb = '你的WebHook' --IG
+Config.Crypto = '你的WebHook' --車庫
+Config.BankTrasnfer = '你的WebHook' --銀行轉帳
+Config.BankLimit = 0 -- #銀行紀錄最低金額
+-- Discord WebHook 設置結束 --
+
+
+
+-- 語音系統更改(如與自述文件的依賴符合，不需更改) --
+
+Config.UseMumbleVoIP    = true -- 使用 Frazzles Mumble-VoIP 資源（推薦！） https://github.com/FrazzIe/mumble-voip
+Config.PMAVoice         = false
+Config.UseTokoVoIP      = false
+Config.SaltyChat        = false
+
+Config.CallPhone        = true  -- 如果玩家不在遊戲中或者遊戲中沒有物品，則會發出警告。
+
+-- 手機設置-完成--
+
+Config.ValePrice        = 100    -- Vale Price
+Config.TaxiPrice        = 75     -- Taxi Price ( 75$/KM )
---
+++ locales/en.lua
@@ -0,0 +1,93 @@
+Locales['en'] = {
+        ['phone_ring'] = "你的手機正在響鈴",
+        ['new_one_message'] = "新訊息",
+        ['new_message_hotline'] = "熱線有新訊息",
+        ['new_message_normal'] = "來自新訊息",
+        ['offline_call'] = "你正在呼叫的人目前無法接通。",
+        ['nophone_call'] = "你正在呼叫的人已掛斷電話。",
+        ['blcknumber_call'] = "對方已封鎖你",
+        ["no_item"] = "沒有手機",
+        ["bank_noplayer"] = "請確認你輸入了正確的資訊",
+        ["phone_time"] = "稍後再試",
+    
+        -- Vale
+        ["vale_get"] = "你以 $100 購買了你的車",
+        ["vale_gete"] = "一次只能使用一次代客泊車服務。",
+        ["vale_getr"] = "你的車輛已在地圖上標記。",
+        ["vale_checmoney"] = "你欠政府超過 $10，直到支付罰款前，你不能取回你的車輛！",
+        ["vale_fee"] = "代客泊車費",
+    
+        ["car_seller"] = "由於你的車輛已出售，你的車輛被拖走了。",
+        ["car_purchase"] = "汽車購買",
+        ["car_sale"] = "汽車銷售",
+        -- cyrpto
+        ["not_enough_money"] = "你的金錢不足",
+        ["not_enough_coin"] = "你的虛擬幣不足",
+        ["cyrpto_buy"] = "你購買了 ",
+        ["cyrpto_sell"] = "你賣出了 ",
+        ["not_crypto"] = "你必須先購買",
+        ["cyrpto_transfer"] = "轉帳成功",
+        ["cyrpto_transfer2"] = "虛擬幣已轉入你的帳戶",
+        ["cyrpto_check"] = "檢查數量",
+        ["cyrpto_notplayer"] = "未找到電話號碼",
+    
+        -- JOB Notifi
+        ["not_player"] = "在職業單位中找不到活躍的人",
+        ["job_notifi"] = "系統收到新通知",
+    
+        -- Race
+        ["race_distance"] = "你離目標地點太遠，請前往標記區域",
+    
+        -- Billing
+        ["bill_paid"] = "帳單已支付 $",
+        ["bill_nocash"] = "你的金錢不足。",
+    
+        -- Billing
+        ["carseller_ownbuy"] = "你不能購買自己的車輛",
+        ["carseller_nobank"] = "你的銀行帳戶中金錢不足",
+    
+        ["carseller_soldvehicle"] = "你的車輛已售出",
+        ["carseller_buyvehicle"] = "你已購買車輛。祝你使用愉快",
+    
+        -- Group Message
+        ["group_created"] = "群組已建立",
+        ["group_deleted"] = "該群組已被刪除。",
+        ["group_newc"] = "已新增新聯絡人",
+        ["group_newmes"] = "群組內有新訊息",
+        ["group_newpeop"] = "已將新成員加入群組",
+        ["group_delpeop"] = "已退出",
+    
+        -- Bank
+        ["bank_yourself"] = "你不能轉帳給自己！",
+        ["bank_nomoney"] = "轉帳金錢不足！",
+        ["bank_transfer"] = "銀行轉帳已完成",
+        ["bank_playertransfer"] = "銀行轉帳來自 ",
+        ["bank_systemnophone"] = "電話號碼未在系統中註冊。",
+    
+        -- Taxi
+        ["taxi_customer"] = "已找到乘客",
+        ["taxi_noemployees"] = "沒有司機",
+        ["taxi_nomoney"] = "你的金錢不足",
+        ["taxi_gotjob"] = "已接受工作",
+        ["taxi_driverway"] = "出租車司機正在前往 ",
+        ["taxi_driverpicked"] = "出租車司機已在你的位置接你",
+        ["taxi_customerleft"] = "你已把乘客帶到目的地",
+        ["taxi_earnings"] = "出租車收入",
+        ["taxi_payment"] = "出租車支付",
+        ["taxi_cancel"] = "出租車取消",
+        ["taxi_customercar"] = "你必須在車裡才能接客",
+        ["taxi_customernotveh"] = "乘客不在你的車內",
+        ["taxi_customernotveh2"] = "乘客不在車內",
+        ["taxi_notcar"] = "你不在車內",
+        ["taxi_customerloc"] = "你已把乘客帶到目標地點",
+    
+        --Mail
+        ["email_received"] = "收到來自某人的電子郵件",
+    
+        --Tinder
+        ["tinder_matched"] = "你已配對",
+        ["tinder_notusername"] = "此使用者名稱已被其他人使用。",
+        ["tinder_sucess"] = "你的帳戶已建立。祝你找到好的配對：）",
+        ["tinder_notlogin"] = "登錄失敗，信息錯誤！",
+        ["tinder_login"] = "已登錄",
+}
